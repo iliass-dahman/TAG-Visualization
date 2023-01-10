42b97b67fc1d45eef3d82c633d8afd848f1a6c4b
ZIANE HICHAM <ziane.hicham.10@gmail.com>
2023-01-10 02:59:50 +0100

    Streaming_Final3

--- python/consumer/run_consumer.sh
+++ python/consumer/run_consumer.sh
@@ -1,5 +1,4 @@
-sleep 120s 
-python3 ./producer.py &
+sleep 140s 
 python3 ./consumer.py &
 sleep 30s 
 python3 ./stat1.py &
--- python/producer/run_producer.sh
+++ python/producer/run_producer.sh
@@ -1,7 +1,2 @@
 sleep 120s 
-python3 ./producer.py &
-python3 ./consumer.py &
-sleep 30s 
-python3 ./stat1.py &
-python3 ./stat2.py &
-python3 ./top_line.py 
+python3 ./producer.py 
