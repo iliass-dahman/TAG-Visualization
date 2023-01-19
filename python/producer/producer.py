cc8b803aa05c6ab3a0c2af1132174be9625aeb5d
obaydah bouifadene <obaydah.bouifadene@um5r.ac.ma>
2023-01-19 20:48:56 +0100

    dynamically reads env variables

--- python/producer/producer.py
+++ python/producer/producer.py
@@ -4,17 +4,19 @@ from time import sleep
 from kafka import KafkaProducer
 
 from model import Validation, Client, Card, Lines
+from producer_config import BROKER_URL, KAFKA_VALIDATION_TOPIC
 
 producer = None
 while True:
     try:
         print("Connecting to the Kafka..........")
-        producer = KafkaProducer(bootstrap_servers='broker:9092')
+        producer = KafkaProducer(bootstrap_servers=BROKER_URL)
         break
     except:
+        sleep(1)
         print("Connection to the Kafka failed, retrying in 5 seconds")
 
-print("Connected to the Kafka"+str(producer))
+print("Connected to the Kafka" + str(producer))
 s = "hello"
 while True:
     print("Sending message to the Kafka..........")
@@ -22,7 +24,7 @@ while True:
     print(line)
     validation = Validation(Client.random(), Card.random(), line.random_station(), line.id, line.random_station(),
                             datetime.now().isoformat())
-    print(validation)             
-    producer.send('temp-topic', bytes(validation.to_json(), encoding='utf-8'))
+    print(validation)
+    producer.send(KAFKA_VALIDATION_TOPIC, bytes(validation.to_json(), encoding='utf-8'))
     print("Message sent to the Kafka..........")
     sleep(5)
