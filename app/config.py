e8784caa617a94436a088f9ede1fe7bb58c89dae
obaydah bouifadene <obaydah.bouifadene@um5r.ac.ma>
2023-01-24 19:12:13 +0100

    fixed err

--- app/config.py
+++ app/config.py
@@ -4,5 +4,5 @@ kafka_url = os.getenv('BROKER_NAME') + ":" + os.getenv('BROKER_PORT')
 kafka_subs_topic = os.getenv('BROKER_SUBSCRIPTION_TOPIC')
 kafka_usage_topic = os.getenv('BROKER_USAGE_TOPIC')
 CASSANDRA_PORT = os.getenv('CASSANDRA_PORT')
-CASSANDRA_SERVICE_NAME = {os.getenv('CASSANDRA_URL')}
+CASSANDRA_SERVICE_NAME = os.getenv('CASSANDRA_URL')
 CASSANDRA_KEYSPACE = os.getenv('CASSANDRA_KEYSPACE')
